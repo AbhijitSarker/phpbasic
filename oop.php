8c68898373bfb41de4454609604d60e26635550a
Abhijitsarker <abhijitsarker03@gmail.com>
2022-09-18 22:59:56 +0600

    oop

--- oop.php
+++ oop.php
@@ -1,7 +1,18 @@
 <?php
 
     Class Myclass{
-        
+        public $x, $y, $z; 
+
+        public function mysum(){
+            $this->z = $this->x + $this->y;
+            return $this->z;
+        }
     }
 
+    $asib = new Myclass();
+    $asib->x = 100;
+    $asib->y = 150;
+
+    echo $asib->mysum();
+ 
 ?>
